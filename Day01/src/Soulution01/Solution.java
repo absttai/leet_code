cd7527e0dfebd926c181dfabb6507e34256cd443
“regesi” <54320371@qq.com>
2021-12-31 01:22:30 +0800

    hello

---
+++ Day01/src/Soulution01/Solution.java
@@ -0,0 +1,30 @@
+package Soulution01;
+
+import java.util.HashSet;
+
+/**
+ * 给定一个整数数组，判断是否存在重复元素。
+ * <p>
+ * 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ * @author Regesi
+ * @date 2021/12/30 22:49
+ **/
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 5};
+        boolean result = containsDuplicate(nums);
+        System.out.println(result);
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> newArray = new HashSet<>();
+        for (int num : nums) {
+            if (!newArray.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Day01/src/Soulution02/Solution.java
@@ -0,0 +1,36 @@
+package Soulution02;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int result = maxSubArray(nums);
+        System.out.println(result);
+    }
+
+    /**
+     * 动态规划解法
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray(int[] nums) {
+        //数组长度
+        int len = nums.length;
+        //新数组用来存子序列的和
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        for (int i = 1; i < len; i++) {
+            if (dp[i - 1] > 0) {
+                //dp[i]表示以nums中以nums[i]结尾的最大子序列的和
+                //如果为复数下一个元素与当前元素相加不能大于下一个元素，所以抛弃
+                dp[i] = dp[i - 1] + nums[i];
+            }else{
+                dp[i]=nums[i];
+            }
+        }
+        int res=dp[0];
+        for (int i = 0; i < len; i++) {
+            res=Math.max(res,dp[i]);
+        }
+        return res;
+    }
+}
